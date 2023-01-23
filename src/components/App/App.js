56342e3858d369b20f64f30274fb5985f3692676
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2023-01-23 15:08:29 +0300

    начало

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import AppHeader from "./AppHeader.js";
+/*import BurgerConstructor from "./BurgerConstructor.js";*/
+//import BurgerIngredients from "./BurgerIngredients.js";
+
+
+function App() {
+
+   
+  
+
+  return (
+    
+    <div>
+      <AppHeader  />
+   
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/App/BurgerConstructor.js
@@ -0,0 +1,11 @@
+/*import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';*/
+
+function BurgerConstructor() {
+  return (
+    <div className="BurgerConstructor">
+     
+    </div>
+  );
+}
+
+export default BurgerConstructor;
